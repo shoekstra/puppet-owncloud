3a247fe6cd691ee75e73d9605a7c70d6b8220015
Stephen Hoekstra <stephenhoekstra@gmail.com>
2016-06-05 00:31:01 +0200

    Added some basic acceptance tests

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -6,18 +6,17 @@ describe 'owncloud class' do
     # Using puppet_apply as a helper
     it 'should work idempotently with no errors' do
       pp = <<-EOS
-      class { 'mysql::server':
+      class { '::mysql::server':
         override_options => {
           'mysqld' => { 'bind-address' => '0.0.0.0' }
         },
         restart       => true,
-        root_password => 'sup3rt0ps3cr3t',
+        root_password => 'password',
       }
 
-      class { 'owncloud':
-        db_user => 'owncloud',
-        db_pass => 'p4ssw0rd',
-      }
+      class { '::owncloud': }
+
+      Class['::mysql::server'] -> Class['::owncloud']
       EOS
 
       # Run it twice and test for idempotency
@@ -25,8 +24,24 @@ describe 'owncloud class' do
       apply_manifest(pp, :catch_changes  => true)
     end
 
-    describe package('owncloud-server') do
+    describe package('owncloud-files') do
       it { is_expected.to be_installed }
+    end
+
+    describe command('php -m') do
+      %w(ctype dom GD iconv JSON libxml mb posix SimpleXML XMLWriter zip zlib).each do |mod|
+        its(:stdout) { should match /#{mod}/i }
+      end
+    end
+
+    describe command('curl -Is http://localhost | head -n 1') do
+      its(:stdout) { should match /200 OK/ }
+    end
+
+    describe command('curl -s http://localhost') do
+      its(:stdout) { should match /\<title\>.*ownCloud.*\<\/title\>/m } # Test title is correct
+      its(:stdout) { should match /placeholder="Username"/ } # Test username field box exists
+      its(:stdout) { should match /placeholder="Password"/ } # Test password field box exists
     end
   end
 end
